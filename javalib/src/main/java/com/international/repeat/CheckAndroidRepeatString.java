cdc9c8fed6e3a02ce85431f2ce18bea70201fdd1
AlexShen <hongshen@2dfire.com>
2018-05-03 14:14:59 +0800

    查询android中key重复value不同的字符串

--- javalib/src/main/java/com/international/repeat/CheckAndroidRepeatString.java
+++ javalib/src/main/java/com/international/repeat/CheckAndroidRepeatString.java
@@ -1,6 +1,8 @@
 package com.international.repeat;
 
 import com.international.repeat.vo.StringXmlBean;
+import com.international.util.InternationalFileUtils;
+
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -8,19 +10,22 @@ import org.w3c.dom.NodeList;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
 /**
- * 查找项目中String文件中key相同的字符串
+ * 检测项目所有String文件中key相同的字符串
  * @author : hongshen
  * @Date: 2018/4/27 0027
  */
 public class CheckAndroidRepeatString {
 
-    private static final String path = "D:/translateTemp/replace2018.04.27";
+    private static final String singlePath = "D:/translateTemp/replace2018.04.27";
+
+    private static final String multiPath = "D:/AndroidStudio/AndroidProject/Checkout4";
 
     /**
      * 所有文件的所有String字符串对象
@@ -29,7 +34,8 @@ public class CheckAndroidRepeatString {
 
     public static void main(String[] args){
         CheckAndroidRepeatString checkAndroidRepeatString = new CheckAndroidRepeatString();
-        checkAndroidRepeatString.checkFolder();
+        checkAndroidRepeatString.checkOnMultiFolder(multiPath, "strings.xml");
+//        checkAndroidRepeatString.checkOnSingleFolder();
         checkAndroidRepeatString.printRepeatValue();
     }
 
@@ -37,8 +43,8 @@ public class CheckAndroidRepeatString {
      * 查找文件
      * 读取一个文件夹内的所有文件
      */
-    private void checkFolder(){
-        File file = new File(path);
+    private void checkOnSingleFolder(){
+        File file = new File(singlePath);
         if (file.isDirectory()) {
             File[] files = file.listFiles();
             if (files != null) {
@@ -46,6 +52,43 @@ public class CheckAndroidRepeatString {
                     List<StringXmlBean> beanList = splitStringXml2Map(f);
                     if (beanList != null) {
                         allList.addAll(beanList);
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * 查找文件
+     * 读取一个路径下多个文件夹的指定文件
+     */
+    private void checkOnMultiFolder(String baseDirName, String targetFileName){
+        File baseDir = new File(baseDirName);       // 创建一个File对象
+        if (!baseDir.exists() || !baseDir.isDirectory()) {  // 判断目录是否存在
+            System.out.println("文件查找失败：" + baseDirName + "不是一个目录！");
+        }
+        String tempName = null;
+        //判断目录是否存在
+        File tempFile;
+        File[] files = baseDir.listFiles();
+        for (int i = 0; i < files.length; i++) {
+            tempFile = files[i];
+            if(tempFile.isDirectory()){
+                checkOnMultiFolder(tempFile.getAbsolutePath(), targetFileName);
+            }else if(tempFile.isFile()){
+                //只扫描values文件夹
+                String parent = tempFile.getParent();
+                boolean isValues = parent.endsWith("values");
+                if (isValues) {
+                    tempName = tempFile.getName();
+                    if(InternationalFileUtils.wildcardMatch(targetFileName, tempName)){
+                        // 匹配成功，将文件名添加到结果集
+//                    fileList.add(tempFile.getAbsoluteFile());
+//                        System.out.println("fileName = " + tempFile.getName() + "\nAbsolutePath = " + tempFile.getAbsolutePath() + "\n");
+                        List<StringXmlBean> beanList = splitStringXml2Map(tempFile);
+                        if (beanList != null) {
+                            allList.addAll(beanList);
+                        }
                     }
                 }
             }
@@ -64,7 +107,9 @@ public class CheckAndroidRepeatString {
                 StringXmlBean beanj = allList.get(j);
                 String keyi = beani.getKey();
                 String keyj = beanj.getKey();
-                if (keyi.equals(keyj)) {
+                String valuei = beani.getValue();
+                String valuej = beanj.getValue();
+                if (keyi.equals(keyj) && !valuei.equals(valuej)) {
                     if (set.add(beani)) {
                         repeatList.add(beani);
                     }
@@ -74,8 +119,25 @@ public class CheckAndroidRepeatString {
                 }
             }
         }
-        for (StringXmlBean bean : repeatList) {
-            System.out.println("重复的 keyi = " + bean.getKey() + "\n 值 = " + bean.getValue() + "\n 文件名 = " + bean.getFileName() + "\n");
+//        for (StringXmlBean bean : repeatList) {
+//            System.out.println("key = " + bean.getKey() + "\nvalue = " + bean.getValue() + "\nfilePath = " + bean.getFileName() + "\n");
+//        }
+        printRepeatKey(repeatList);
+    }
+
+    /**
+     * 打印出有多少个重复的key
+     */
+    private void printRepeatKey(List<StringXmlBean> repeatList){
+        Set<String> keySet = new HashSet<>();
+        for (int i = 0; i < repeatList.size()-1; i++) {
+            String keyi = repeatList.get(i).getKey();
+            keySet.add(keyi);
+        }
+        Iterator<String> iterator = keySet.iterator();
+        while (iterator.hasNext()){
+            String key = iterator.next();
+            System.out.println("key = " + key);
         }
     }
 
